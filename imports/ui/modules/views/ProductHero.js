b7559bcaf3b143f0e34e5f6f38fe9b796f9a1bff
Matthew Podolak <mpodola2@gmail.com>
2020-02-11 21:38:56 -0500

    trying to add favicon

--- imports/ui/modules/views/ProductHero.js
+++ imports/ui/modules/views/ProductHero.js
@@ -37,7 +37,7 @@ function ProductHero(props) {
       {/* Increase the network loading priority of the background image. */}
       <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
       <Typography color="inherit" align="center" variant="h2" marked="center">
-        Sunshine Sketches
+        Sunshine Sketches Homecoming
       </Typography>
       <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
         {/* Enjoy secret offers up to -70% off the best luxury hotels every Sunday. */}
